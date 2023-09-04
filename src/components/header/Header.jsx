595d626e63659691c89c404fa233fb9120fb0fb2
Raylanelopesz <59022610+Raylanelopesz@users.noreply.github.com>
2023-09-03 21:27:09 -0300

    responsividade

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logoo.png";
-import { links } from "../../Data"
+import { links } from "../../Data";
+import { animateScroll } from "react-scroll";
 import { FaStream } from "react-icons/fa";
 import "./header.css";
+import { Link } from "react-scroll"
+import { scrollToTop } from "react-scroll/modules/mixins/animate-scroll";
 
 function Header() {
+    const [scrollHeader, setScrollHeader] = useState(false);
 
+    const changeHeader = () => {
+        if (window.scrollY >= 80) {
+            setScrollHeader(true);
+        } else {
+            setScrollHeader(false)
+        }
+    };
+
+    const scrollTop = () => {
+        animateScroll.scrollToTop()
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeHeader)
+    }, [])
     return (
-        <header className="header">
+        <header className={`${scrollHeader ? 'scroll-header' : ''}
+        header`}>
             <nav className="nav container">
-                <a href="" className="nav__logo">
+                <Link to='/' onClick={scrollToTop} className="nav__logo">
                     <img src={logo} alt="" className="nav__logo-img" />
-                </a>
+                </Link>
 
                 <div className="nav__menu">
                     <ul className="nav__list">
                         {links.map(({ name, path }, index) => {
                             return (
                                 <li className="nav__item" key={index}>
-                                    <a href={path} className="nav__link">
+                                    <Link
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-60}
+                                        hashSpy={true}
+                                        duration={500}
+                                        to={path} className="nav__link">
                                         {name}
-                                    </a>
+                                    </Link>
                                 </li>
                             )
                         })}
